7aeda4fa83ed6eea2da81cb3d901523e2cc69779
Michael Mount <mount.photos@gmail.com>
2024-01-18 17:28:07 -0700

    moveed logos down and added link to new site

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,34 +1,12 @@
 import React, { useState, useEffect } from "react";
-import lightLogo from "./images/10_5logo_white.png";
-import darkLogo from "./images/10_5logo_black.png";
+
 
 export default function NavBar() {
-  const [logo, setLogo] = useState(
-    document.documentElement.getAttribute("data-theme") === "mydefault"
-      ? lightLogo
-      : darkLogo
-  );
-
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      const currentTheme = document.documentElement.getAttribute("data-theme");
-      if (currentTheme === "mydefault" && logo !== lightLogo) {
-        setLogo(lightLogo);
-      } else if (currentTheme !== "mydefault" && logo !== darkLogo) {
-        setLogo(darkLogo);
-      }
-    }, 1000);
-
-    return () => clearInterval(intervalId);
-  }, [logo]);
+ 
 
   return (
     <nav className="py-10 px-4 sm:px-6 md:mx-20 md:my-16 flex justify-between items-center">
-      {/* Logo and home link */}
-      <a href="/" className="flex items-center">
-        <img src={logo} alt="Logo" className="h-12 sm:h-16 w-auto" />
-      </a>
-
+    
       {/* Responsive menu */}
       <div className="text-right">
         <a
--- src/components/pages/splash.js
+++ src/components/pages/splash.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from "react";
+import SOGlog from "../images/SOGType.png"
+import TAF from "../images/10_5logo_white.png"
 
 export default function Splash() {
   const [theme, setTheme] = React.useState("mydefault");
@@ -12,35 +14,14 @@ export default function Splash() {
 
   return (
     <>
-      
-
-      <div className="flex flex-col mx-6 md:mx-24">
-      <input onClick={toggleTheme} className="toggle md:relative md:left-3/4  md:top-10 " type="checkbox" />
-        <div className="text-4xl mt-10 md:text-9xl font-oswald tracking-tighter">
-          <h1>
-            It's not about the hours <br /> that we keep,
-          </h1>
-        </div>
-        <div className="text-xl left-16 p-5 md:text-4xl relative md:bottom-20 md:left-96 pl-4 md:pl-44 mt-2 font-architects ">
-          <h1>
-            but our mantra for making <br />
-            lasting impressions.{" "}
-          </h1>
-        </div>
+      <div className="flex flex-col justify-center items-center h-scree">
+      <img src={SOGlog} alt="Logo" className="h-14 sm:h-36 w-auto" />
+      <h2 className="text-5xl sm:text-7xl font-oswald">Is Now</h2>
+      <img src={TAF} alt="Logo" className="h-16 sm:h-36 w-auto  m-5 sm:m-10" />
+      <a href="https://10and5creative.com/" className="sm:text-3xl text-2xl font-oswald underline">See Our New site</a>
       </div>
+     
 
-
-      <div className="flex flex-col md:flex-row justify-center md:space-x-10 my-8 space-y-">
-        <h1 className="border  px-4 py-2 md:px-20 md:py-4 font-bricolage md:mb-0 text-center">
-          BRAND IDENTITY
-        </h1>
-        <h1 className="border px-4 py-2 md:px-20 md:py-4 font-bricolage md:mb-0 text-center">
-          CONTENT STRATEGY
-        </h1>
-        <h1 className="border  px-4 py-2 md:px-20 md:py-4 font-bricolage md:mb-0 text-center ">
-          CREATIVE DEVELOPMENT
-        </h1>
-      </div>
     </>
   );
 }
